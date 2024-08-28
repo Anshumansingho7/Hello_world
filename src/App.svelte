e08401ceb955441572bce5d30966ba269994fdbd
Anshumansingho7 <anshumansinghshaktawat09@gmail.com>
2024-08-28 15:01:20 +0530

    each block

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,17 @@
 <script>
+	const names = [
+		{ firstName: 'Tony', lastName: 'Stark', movies: 10, age: 45 },
+		{ firstName: 'Steve', lastName: 'Rogers', movies: 9, age: 100 },
+		{ firstName: 'Thor', lastName: 'Odinson', movies: 8, age: 1500 }
+	]
 </script>
 
 <main>
-	
+	{#each names as { firstName, lastName, movies, age }, index}
+		<h1>{index + 1}. {firstName} {lastName}</h1>
+		<p>Movies: {movies}</p>
+		<p>Age: {age}</p>
+	{/each}
 </main>
 
 <style>
@@ -12,5 +21,14 @@
 		padding: 1em; /* Adds padding inside the container */
 		max-width: 240px; /* Sets the maximum width of the container */
 		margin: 0 auto; /* Centers the container horizontally on the page */
+	}
+
+	h1 {
+		margin: 0.5em 0; /* Adds some space around the heading */
+	}
+
+	p {
+		margin: 0.2em 0; /* Adds some space around the paragraph */
+		font-size: 1em; /* Adjusts the font size */
 	}
 </style>
